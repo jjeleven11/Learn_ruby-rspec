a12df31a0f7b6fd912dcd0f72f04e8df76ecb8f8
jjeleven11 <40149119+jjeleven11@users.noreply.github.com>
2018-07-09 15:25:19 +0200

    Create calculator.rb

---
+++ calculator.rb
@@ -0,0 +1,20 @@
+def add (a, b)
+     (a + b);
+end
+
+def subtract (a, b)
+  (a - b);
+end
+
+def sum(nbre = [])
+   nbre.reduce(0,:+)
+end
+
+def multiply (a, b)
+    a * b
+
+end
+
+def power (a, b)
+  a ** b
+end
